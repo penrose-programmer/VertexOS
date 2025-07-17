641ac72ec2f376b8036a28b4c531cfd7c04f53de
System administrator <syedtalhahahmed@gmail.com>
2025-07-17 11:32:41 +1000

    Initial Commit

---
+++ configuration.nix
@@ -0,0 +1,88 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+      # Home Manager comes from flake.nix now
+    ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Hostname
+  networking.hostName = "nixos";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Time zone and locale
+  time.timeZone = "Australia/Sydney";
+
+  i18n.defaultLocale = "en_AU.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+
+  # Enable X11 and GNOME
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Keyboard layout
+  services.xserver.xkb = {
+    layout = "au";
+    variant = "";
+  };
+
+  # Enable printing
+  services.printing.enable = true;
+
+  # Enable sound (PipeWire)
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Users
+  users.users.penrose = {
+    isNormalUser = true;
+    description = "Talhah Ahmed";
+    extraGroups = [ "networkmanager" "wheel" ];
+    # Don’t put user-level packages here anymore; Home Manager handles it
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # System-wide packages
+  environment.systemPackages = with pkgs; [
+    vscode
+    wget
+    obsidian
+  ];
+
+  # Programs
+  programs.firefox.enable = true;
+  programs.git.enable = true;
+  programs.hyprland.enable = true;
+
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # System state version
+  system.stateVersion = "25.05";
+}
