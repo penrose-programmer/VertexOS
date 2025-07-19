66b61e818a75f42e4336ef827f595f5feca732d1
penrose <syedtalhahahmed@gmail.com>
2025-07-20 07:31:12 +1000

    Fixed Deprecation

--- configuration.nix
+++ configuration.nix
@@ -34,8 +34,8 @@
 
   # Enable X11 and GNOME
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Keyboard layout
   services.xserver.xkb = {
