59d4df035016d840ec0adfa97ee8bcf07033af77
penrose <syedtalhahahmed@gmail.com>
2025-07-21 08:15:51 +1000

    Audio Shortcut

--- home.nix
+++ home.nix
@@ -381,9 +381,20 @@
         9)
       );
 
+      hyprpaper.
+
       bindm = [
         "$mod, mouse:273, resizewindow"
         "$mod, mouse:272, movewindow"
+      ];
+
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
       ];
 
       exec-once = [ 
