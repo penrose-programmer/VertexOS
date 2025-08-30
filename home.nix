3a59494bbfe43bf09bfd91df16131da716dd6c40
penrose-programmer <syedtalhahahmed@gmail.com>
2025-08-31 07:46:43 +1000

    pause/play mpvpaper

--- home.nix
+++ home.nix
@@ -375,6 +375,8 @@
         "$mod, Z, exec, rofi -show run -theme Arc-Dark"
         "$mod, X, exec, zeditor"
         "$mod, C, exec, thunar"
+        "$mod, P, exec, pkill -STOP mpvpaper"
+        "$mod_s, P, exec, pkill -CONT mpvpaper"
       ] ++ (
         # workspaces
         # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
