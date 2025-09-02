db01d1fa94baf14a2e1a7964c766e11335ee980a
penrose-programmer <syedtalhahahmed@gmail.com>
2025-09-03 06:24:27 +1000

    fix

--- home.nix
+++ home.nix
@@ -34,7 +34,7 @@
 
   stylix.enable = true;
   stylix.targets.waybar.enable = false;
-  stylix.targets.firefox.profileNames = [ "default" ];
+  stylix.targets.firefox.colorTheme.enable = true;
 
   programs.bash = {
     enable = true;
