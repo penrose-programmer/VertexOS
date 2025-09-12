b92f0bd658d6bdeaf4367da5c2f1e38d3e53b6ac
penrose-programmer <syedtalhahahmed@gmail.com>
2025-09-13 08:15:07 +1000

    fix

--- home.nix
+++ home.nix
@@ -40,8 +40,6 @@
     enableCompletion = true;
     bashrcExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-
-      eval "$(zoxide init bash)"
     '';
 
     # set some aliases, feel free to add more or remove some
