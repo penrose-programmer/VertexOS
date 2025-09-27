7a74088998cf8718d47eb8e0f590b0f58f25cfb5
penrose-programmer <syedtalhahahmed@gmail.com>
2025-09-28 06:22:14 +1000

    fix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS with Home Manager, Stylix, and Textfox";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
