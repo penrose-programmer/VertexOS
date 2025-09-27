81fc3eebd9b31b56639a3ac3ccede240f1bd2ffd
penrose-programmer <syedtalhahahmed@gmail.com>
2025-09-28 06:24:35 +1000

    fix

--- configuration.nix
+++ configuration.nix
@@ -36,8 +36,8 @@
 
   # Enable X11 and GNOME
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = false;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = false;
 
   services.libinput.touchpad.naturalScrolling = true;
 
