a9893bfb2f670577a89afa85ec2c157d81c4ae35
penrose-programmer <syedtalhahahmed@gmail.com>
2025-09-29 09:19:28 +1000

    fix

--- nixvim/flake.nix
+++ nixvim/flake.nix
@@ -32,7 +32,7 @@
             module = import ./config; # import the module directly
             # You can use `extraSpecialArgs` to pass additional arguments to your module files
             extraSpecialArgs = {
-              # inherit (inputs) foo;
+              interhit inputs;
             };
           };
           nvim = nixvim'.makeNixvimWithModule nixvimModule;
