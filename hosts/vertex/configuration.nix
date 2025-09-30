72bf8b7e29a2ce301cc2d4daee43b21e6051ae9b
penrose-programmer <syedtalhahahmed@gmail.com>
2025-10-01 09:00:59 +1000

    stylix

--- hosts/vertex/configuration.nix
+++ hosts/vertex/configuration.nix
@@ -72,9 +72,23 @@
     enable = true;
     autoEnable = false;
 
-    image = pkgs.fetchurl {
-      url = "https://wallpapers.com/images/high/rain-world-creature-encounter-mbw3d33fkvsproi4.webp";
-      hash = "sha256-jISNr/0ME1amHJ8ywSULyugbMPnZtBaHqbBK/HsF2OM=";
+    base16Scheme = {
+      base00 = "212121";
+      base01 = "303030";
+      base02 = "353535";
+      base03 = "4A4A4A";
+      base04 = "B2CCD6";
+      base05 = "EEFFFF";
+      base06 = "EEFFFF";
+      base07 = "FFFFFF";
+      base08 = "F07178";
+      base09 = "F78C6C";
+      base0A = "FFCB6B";
+      base0B = "C3E88D";
+      base0C = "89DDFF";
+      base0D = "82AAFF";
+      base0E = "C792EA";
+      base0F = "FF5370";
     };
 
     cursor.package = pkgs.bibata-cursors;
