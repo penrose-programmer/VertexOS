473b4751c4e8e825750602217df267beb522f54d
penrose-programmer <syedtalhahahmed@gmail.com>
2025-09-30 18:17:29 +1000

    kitty themes:

--- home.nix
+++ home.nix
@@ -36,14 +36,51 @@
     colorschemes.oxocarbon.enable = true;
 
     extraPlugins = with pkgs.vimPlugins; [
-	nvim-treesitter
-	neo-tree-nvim
-	nvim-web-devicons
+      nvim-treesitter
+      neo-tree-nvim
+      nvim-web-devicons
     ];
+
+    globalOpts = {
+      expandtab = true;
+      # tabstop = 4;
+      shiftwidth = 2;
+      # softtabstop = 0;
+      # smarttab = true;
+    }
 
     clipboard.register = "unnamedplus";
 
   };
+
+  
+	programs.kitty = {
+	  enable = true;
+
+	  font.name = "FiraCode Nerd Font";
+	  font.size = 12;
+
+	  settings = {
+	    background = "#161616";
+	    foreground = "#f2f4f8";
+	    color0  = "#262626";
+	    color1  = "#ff7eb6";
+	    color2  = "#42be65";
+	    color3  = "#ffe97b";
+	    color4  = "#78a9ff";
+	    color5  = "#be95ff";
+	    color6  = "#33b1ff";
+	    color7  = "#dfdfe0";
+	    color8  = "#393939";
+	    color9  = "#ff7eb6";
+	    color10 = "#42be65";
+	    color11 = "#ffe97b";
+	    color12 = "#78a9ff";
+	    color13 = "#be95ff";
+	    color14 = "#33b1ff";
+	    color15 = "#ffffff";
+	  };
+	};
 
   programs.bash = {
     enable = true;
