8fad4fe7d45f262b50f228abf3dab78530010ab9
penrose-programmer <syedtalhahahmed@gmail.com>
2025-10-01 12:11:39 +1000

    kanata

--- nixosModules/services/kanata.nix
+++ nixosModules/services/kanata.nix
@@ -1,32 +1,28 @@
-{ pkgs, ... }: {
+{ lib, config, pkgs, ... }:
   services.kanata = {
     enable = true;
     keyboards = {
       internalKeyboard = {
-        devices = [
-          "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
-          "/dev/input/by-id/usb-Framework_Laptop_16_Keyboard_Module_-_ANSI_FRAKDKEN0100000000-event-kbd"
-          "/dev/input/by-id/usb-Framework_Laptop_16_Keyboard_Module_-_ANSI_FRAKDKEN0100000000-if02-event-kbd"
-        ];
+        devices = [];
         extraDefCfg = "process-unmapped-keys yes";
         config = ''
           (defsrc
            caps a s d f j k l ;
           )
           (defvar
-           tap-time 150
-           hold-time 200
+           tap-time 300
+           hold-time 500
           )
           (defalias
-           caps (tap-hold 100 100 esc lctl)
-           a (tap-hold $tap-time $hold-time a lmet)
-           s (tap-hold $tap-time $hold-time s lalt)
-           d (tap-hold $tap-time $hold-time d lsft)
-           f (tap-hold $tap-time $hold-time f lctl)
-           j (tap-hold $tap-time $hold-time j rctl)
-           k (tap-hold $tap-time $hold-time k rsft)
-           l (tap-hold $tap-time $hold-time l ralt)
-           ; (tap-hold $tap-time $hold-time ; rmet)
+           caps esc
+           a (tap-hold-release-timeout $tap-time $hold-time a lmet a)
+           s (tap-hold-release-timeout $tap-time $hold-time s lalt s)
+           d (tap-hold-release-timeout $tap-time $hold-time d lsft d)
+           f (tap-hold-release-timeout $tap-time $hold-time f lctl f)
+           j (tap-hold-release-timeout $tap-time $hold-time j rctl j)
+           k (tap-hold-release-timeout $tap-time $hold-time k rsft k)
+           l (tap-hold-release-timeout $tap-time $hold-time l ralt l)
+           ; (tap-hold-release-timeout $tap-time $hold-time ; rmet ;)
           )
 
           (deflayer base
