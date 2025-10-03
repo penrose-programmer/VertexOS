2a9e4591eb077424a8567809f61430206f5c97e1
penrose-programmer <syedtalhahahmed@gmail.com>
2025-10-03 15:36:46 +1000

    fprograms.nixvim = {     enable = true;     defaultEditor = true;     nixpkgs.useGlobalPackages = true;
        colorschemes.ayu.enable = true;
    
        lsp.servers = {
          nixd.enable = true;
        };
    
        plugins.cmp = {
          autoEnableSources = true;
          settings.sources = [
            { name = "nvim_lsp"; }
            { name = "path"; }
            { name = "buffer"; }
          ];
        };
    
        plugins.cmp-nvim-lsp.enable = true;
    
        plugins.conform-nvim.enable = true;
    
        extraPlugins = with pkgs.vimPlugins; [
          nvim-treesitter
          neo-tree-nvim
          nvim-web-devicons
          vim-tmux-navigator
        ];
    
        globalOpts = {
          expandtab = true;
          # tabstop = 4;
          shiftwidth = 2;
          # softtabstop = 0;
          # smarttab = true;
        };
    
        clipboard.register = "unnamedplus";
      };ix

--- homeModules/applications/nixvim.nix
+++ homeModules/applications/nixvim.nix
@@ -3,17 +3,19 @@
     nixd
   ];
 
-  programs.nixvim = { 
-    enable = true; 
+  programs.nixvim = {
+    enable = true;
     defaultEditor = true;
     nixpkgs.useGlobalPackages = true;
 
     colorschemes.ayu.enable = true;
 
+    # LSP
     lsp.servers = {
       nixd.enable = true;
     };
 
+    # CMP
     plugins.cmp = {
       autoEnableSources = true;
       settings.sources = [
@@ -23,23 +25,16 @@
       ];
     };
 
-    plugins.cmp-nvim-lsp.enable = true;
-
-    plugins.conform-nvim.enable = true;
-
+    # Extra plugins
     extraPlugins = with pkgs.vimPlugins; [
       nvim-treesitter
       neo-tree-nvim
       nvim-web-devicons
-      vim-tmux-navigator
     ];
 
     globalOpts = {
       expandtab = true;
-      # tabstop = 4;
       shiftwidth = 2;
-      # softtabstop = 0;
-      # smarttab = true;
     };
 
     clipboard.register = "unnamedplus";
