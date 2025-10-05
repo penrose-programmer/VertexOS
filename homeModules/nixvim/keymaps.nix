acb7ca5b75ad967064220ff7300581f40ec86b9e
penrose-programmer <syedtalhahahmed@gmail.com>
2025-10-05 17:33:00 +1100

    telescope binds

--- homeModules/nixvim/keymaps.nix
+++ homeModules/nixvim/keymaps.nix
@@ -3,11 +3,27 @@
     keymaps = [
       {
         mode = "n";
-        key = "<leader>m";
-        action = "<cmd>Neotree toggle<CR>";
-        options = {
-          desc = "Leader Test";
-        };
+        key = "<leader>ff";
+        action = "<cmd>Telescope find_files<CR>";
+        options.desc = "Find files";
+      }
+      {
+        mode = "n";
+        key = "<leader>fg";
+        action = "<cmd>Telescope live_grep<CR>";
+        options.desc = "Live grep";
+      }
+      {
+        mode = "n";
+        key = "<leader>fb";
+        action = "<cmd>Telescope buffers<CR>";
+        options.desc = "Find buffers";
+      }
+      {
+        mode = "n";
+        key = "<leader>fh";
+        action = "<cmd>Telescope help_tags<CR>";
+        options.desc = "Help tags";
       }
     ];
   };
