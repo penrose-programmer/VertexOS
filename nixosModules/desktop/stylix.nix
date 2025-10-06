6309e3805af35b36a9200586e7707143e42fb4a2
penrose-programmer <syedtalhahahmed@gmail.com>
2025-10-07 07:40:25 +1100

    fix

--- nixosModules/desktop/stylix.nix
+++ nixosModules/desktop/stylix.nix
@@ -3,13 +3,26 @@
     enable = true;
     autoEnable = true;
 
-    targets.kitty.enable = false;
-
     base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
 
     cursor.package = pkgs.bibata-cursors;
     cursor.name = "Bibata-Modern-Classic";
     cursor.size = 20;
+    
+    fonts = {
+      monospace = {
+         package = pkgs.jetbrains-mono;
+         name = "JetBrainsMono";
+      };
+      sansSerif = {
+        package = pkgs.rubik;
+        name = "Rubik";
+      };
+      serif = {
+        package = pkgs.noto-fonts;
+        name = "Noto Serif";
+      };
+    };
 
     image = ./space.jpg;
   };
