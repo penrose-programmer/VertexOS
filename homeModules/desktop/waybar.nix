b84c8f1f2dcb5a0a0901dcd5a57d490c9c19dd6e
penrose-programmer <syedtalhahahmed@gmail.com>
2025-10-10 07:21:58 +1100

    fix

--- homeModules/desktop/waybar.nix
+++ homeModules/desktop/waybar.nix
@@ -3,76 +3,133 @@ programs.waybar = {
     enable = true;
     settings = {
       mainBar = {
-        modules-right = [ "network" "pulseaudio" "battery" "clock"];
-        modules-left = [ "hyprland/workspaces" ];
+        "layer": "top",
+        "position": "top",
+        "margin-bottom": -10,
+        "spacing" : 0,
+        "modules-left": [
+          "hyprland/workspaces",
+          "custom/uptime",
+          "cpu",
+        ],
 
-        clock = {
-          interval = 1;
-          format = "{:%r %d/%m/%Y}";
-        };
+        "modules-center": ["clock"],
 
-        "hyprland/window" = {
-          separate-outputs = true;
-        };
+        "modules-right": [
+          "custom/pomodoro",
+          "bluetooth",
+          "network",
+          "pulseaudio", 
+          "backlight",
+          "battery",
+        ],
 
-        "hyprland/workspaces" = {
-          persistent-workspaces = {
-            "*" = 3;
-          };
-        };
+        "hyprland/workspaces": {
+          "format": "{name}: {icon}",
+          "format-icons": {
+            "active": "",
+            "default": ""
+          }
+        },
 
-        pulseaudio = {
-          format = "{volume}% {icon}";
-          format-bluetooth = "{volume}% {icon}";
-          format-muted = "";
-          format-icons = {
-            default = [
-              ""
-              ""
-            ];
-          };
+        "bluetooth": {
+          "format": "󰂲",
+          "format-on": "{icon}",
+          "format-off": "{icon}", 
+          "format-connected":"{icon}",
+          "format-icons":{
+            "on":"󰂯",
+            "off": "󰂲",
+            "connected": "󰂱",
+          },
+          "on-click": "blueman-manager",
+          "tooltip-format-connected":"{device_enumerate}"
+        },
 
-          scroll-step = 1;
-          on-click = "pavucontrol";
-        };
+        "clock": {
+          "timezone": "Asia/Kolkata",
+          "tooltip": false,
+          "format": "{:%H:%M:%S  -  %A, %d}",
+          "interval": 1
+        },
 
-        network = {
-          format = "{ifname}";
-          format-wifi = "{essid}";
-          format-ethernet = "{ipaddr}/{cidr}";
-          format-disconnected = "";
-          on-click = "kitty nmtui";
-        };
+        "network": {
+          "format-wifi": "󰤢",
+          "format-ethernet": "󰈀 ",
+          "format-disconnected": "󰤠 ",
+          "interval": 5,
+          "tooltip-format": "{essid} ({signalStrength}%)",
+          "on-click": "nm-connection-editor"
+        },
 
-        battery = {
-          format = "{capacity}% {icon}";
-          format-charging = " {capacity}%";
-          format-plugged = " {capacity}%";
-          format-alt = "{time} {icon}";
-          format-icons = [
-            "󰂎"
-            "󰁺"
-            "󰁻"
-            "󰁼"
-            "󰁽"
-            "󰁾"
-            "󰁿"
-            "󰂀"
-            "󰂁"
-            "󰂂"
-            "󰁹"
-          ];
+        "cpu": {
+        "interval": 1,
+        "format": "  {icon0}{icon1}{icon2}{icon3} {usage:>2}%",
+        "format-icons": ["▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"],
+        "on-click": "ghostty -e htop"
+        },
 
-          interval = 5;
-          states = {
-            warning = 30;
-            critical = 10;
-          };
+        "memory": {
+        "interval": 30,
+        "format": "  {used:0.1f}G/{total:0.1f}G",
+        "tooltip-format": "Memory"
+        },
 
-          tooltip = false;
-        };
-      };
-    };
+        "custom/uptime": {
+        "format": "{}",
+        "format-icon": [""], 
+        "tooltip": false,
+        "interval": 1600,
+        "exec": "$HOME/.config/waybar/scripts/uptime.sh"
+        },
+
+        "backlight": {
+          "format": "{icon}  {percent}%",
+          "format-icons": ["","󰃜", "󰃛", "󰃞","󰃝","󰃟","󰃠"],
+          "tooltip": false
+        },
+
+        "pulseaudio": {
+        "format": "{icon}  {volume}%",
+        "format-muted": "",
+        "format-icons": {
+        "default": ["", "", " "]
+        },
+        "on-click": "pavucontrol"
+        },
+
+        "battery": {
+          "interval":2,
+          "states": {
+              "good": 95,
+              "warning": 30,
+              "critical": 15
+          },
+          "format": "{icon}  {capacity}%",
+          "format-full": "{icon}  {capacity}%",
+          "format-charging": " {capacity}%",
+          "format-plugged": " {capacity}%",
+          "format-alt": "{icon} {time}",
+          // "format-good": "", // An empty format will hide the module
+          // "format-full": "",
+          "format-icons": ["", "", "", "", ""]
+        },
+
+        "custom/lock": {
+          "tooltip": false,
+          "on-click": "sh -c '(sleep 0s; hyprlock)' & disown",
+          "format" : ""
+        },
+
+        "custom/pomodoro": {
+          "format": "{}",
+          "return-type": "json",
+          "exec": "waybar-module-pomodoro --no-work-icons",
+          "on-click": "waybar-module-pomodoro toggle",
+          "on-click-right": "waybar-module-pomodoro reset",
+        },
+
+      }
 
       style = ''
       * {
