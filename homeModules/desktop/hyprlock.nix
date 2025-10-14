3865681c2ad64269c5964399e086b86ca6b88b3c
penrose-programmer <syedtalhahahmed@gmail.com>
2025-10-14 14:25:54 +1100

    Hyprlock

--- homeModules/desktop/hyprlock.nix
+++ homeModules/desktop/hyprlock.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   programs.hyprlock = {
     enable = true;
     settings = {
@@ -12,6 +12,20 @@
       background = [
         {
           path = "~/Pictures/Wallpaper.jpg";
+        }
+      ];
+
+      input-field = [
+        {
+          size = "500, 50";
+          position = "0, -500";
+          font_color = "#${config.stylix.base16Scheme.base0A}";
+          inner_color = "#${config.stylix.base16Scheme.base00}";
+          outer_color = "#${config.stylix.base16Scheme.base02}";
+          monitor = "";
+          dots_center = true;
+          fade_on_empty = false;
+          placeholder_text = "Password...";
         }
       ];
     };
