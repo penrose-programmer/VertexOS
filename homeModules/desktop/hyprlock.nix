3b4876a4989e02e0b9fefe566a38e7aa12f27860
penrose-programmer <syedtalhahahmed@gmail.com>
2025-10-15 20:21:15 +1100

    fix

--- homeModules/desktop/hyprlock.nix
+++ homeModules/desktop/hyprlock.nix
@@ -11,7 +11,7 @@
 
       background = [
         {
-          path = "~/Pictures/Wallpaper.jpg";
+          path = "~/Pictures/Wallpaper.jpeg";
         }
       ];
 
--- homeModules/nixvim/lsp.nix
+++ homeModules/nixvim/lsp.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }: {
   programs.nixvim = {
-
     plugins.lsp = {
       enable = true;
       inlayHints = true;
