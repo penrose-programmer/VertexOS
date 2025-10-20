bd3a7b1aa9f9754c3b2b04ad9bf3c97c0b33d908
penrose-programmer <syedtalhahahmed@gmail.com>
2025-10-21 10:21:46 +1100

    Fixed lock colour scheme

--- homeModules/desktop/hyprlock.nix
+++ homeModules/desktop/hyprlock.nix
@@ -19,9 +19,10 @@
         {
           size = "500, 50";
           position = "0, -500";
-          font_color = "#${config.stylix.base16Scheme.base0A}";
-          inner_color = "#${config.stylix.base16Scheme.base00}";
-          outer_color = "#${config.stylix.base16Scheme.base02}";
+          font_family = "jetbrains-mono";
+          font_color = "rgba(255, 180, 84, 1.0)";
+          inner_color = "rgba(15, 20, 25, 1.0)";
+          outer_color = "rgba(39, 45, 56, 1.0)";
           monitor = "";
           dots_center = true;
           fade_on_empty = false;
