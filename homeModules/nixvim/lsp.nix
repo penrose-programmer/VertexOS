44bf306138afd66c9cee9c383a9eceb8647179d6
penrose-programmer <syedtalhahahmed@gmail.com>
2025-11-24 06:04:12 +1100

    gdscript lsp

--- homeModules/nixvim/lsp.nix
+++ homeModules/nixvim/lsp.nix
@@ -14,7 +14,8 @@
         gdscript = {
           enable = true;
           package = null;
-          cmd = [ "godot4" "--headless" "--lsp" ];
+          cmd = [ "vim.lsp.rpc.connect" "127.0.0.1" "6005" ];
+          root_dir = ''require("lspconfig.util").root_pattern("project.godot", ".git")'';
         };
       };
     };
