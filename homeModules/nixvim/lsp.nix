28cbaeb8cb2d408af73d47e8d1a1f2f90e328a9b
penrose-programmer <syedtalhahahmed@gmail.com>
2025-11-24 06:04:52 +1100

    fix

--- homeModules/nixvim/lsp.nix
+++ homeModules/nixvim/lsp.nix
@@ -15,7 +15,6 @@
           enable = true;
           package = null;
           cmd = [ "vim.lsp.rpc.connect" "127.0.0.1" "6005" ];
-          root_dir = ''require("lspconfig.util").root_pattern("project.godot", ".git")'';
         };
       };
     };
