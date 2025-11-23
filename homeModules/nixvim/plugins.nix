2e95badda14b32ef5adc1ad3e8b0892956a5265b
penrose-programmer <syedtalhahahmed@gmail.com>
2025-11-24 05:25:29 +1100

    fixed vim godot

--- homeModules/nixvim/plugins.nix
+++ homeModules/nixvim/plugins.nix
@@ -9,11 +9,11 @@
         nvim-cmp
         lazygit-nvim
         vim-be-good
+        vim-godot
       ];
 
       plugins.cmp-nvim-lsp.enable = true;
       plugins.web-devicons.enable = true;
-      plugins.godot.enable = true;
       colorschemes.ayu.enable = true;
     };
 }
