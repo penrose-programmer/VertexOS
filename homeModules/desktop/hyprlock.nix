790fe62af79d7e6bd0876523000a9189f4c38a03
penrose-programmer <syedtalhahahmed@gmail.com>
2025-11-28 07:25:34 +1100

    hyprlock edit

--- homeModules/desktop/hyprlock.nix
+++ homeModules/desktop/hyprlock.nix
@@ -1,55 +1,56 @@
 { config, pkgs, ... }: {
-  programs.hyprlock = {
-    enable = true;
-    settings = {
-      general = {
-        disable_loading_bar = true;
-        grace = 300;
-        hide_cursor = true;
-        no_fade_in = false;
-      };
+programs.hyprlock = {
+enable = true;
+settings = {
+general = {
+disable_loading_bar = true;
+grace = 300;
+hide_cursor = true;
+no_fade_in = false;
+};
 
-      background = [
-        {
-          path = "~/Pictures/samurai.jpg";
-          blur_passes = "2";
-        }
-      ];
+  background = [
+    {
+      path = "~/Pictures/samurai.jpg";
+      blur_passes = "4";
+    }
+  ];
 
-      input-field = [
-        {
-          size = "500, 50";
-          position = "0, -150";
-          font_family = "jetbrains-mono";
-          font_color = config.stylix.base16Scheme.base0E;
-          inner_color = config.stylix.base16Scheme.base00;
-          outer_color = config.stylix.base16Scheme.base01;
-          monitor = "";
-          dots_center = true;
-          fade_on_empty = false;
-          placeholder_text = "Password...";
-          rounding = "0";
-        }
-      ];
+  input-field = [
+    {
+      size = "500, 50";
+      position = "0, -150";
+      font_family = "jetbrains-mono";
+      font_color   = "rgba(187, 154, 247, 1.0)";  # was #BB9AF7
+      inner_color  = "rgba(22, 22, 30, 1.0)";     # was #16161E
+      outer_color  = "rgba(26, 27, 38, 1.0)";     # was #1A1B26
+      monitor      = "";
+      dots_center  = true;
+      fade_on_empty= false;
+      placeholder_text = "Password...";
+      rounding = "0";
+    }
+  ];
 
-      label = [
-        {
-          text = "$TIME";
-          color = config.stylix.base16Scheme.base0D;
-          font_family = "jetbrains-mono";
-          position = "0, 50";
-          font_size = "100";
-          monitor = "";
-        }
-        {
-          text = "Welcome, $USER!";
-          color = config.stylix.base16Scheme.base05;
-          font_family = "jetbrains-mono";
-          position = "0, -50";
-          font_size = "25";
-          monitor = "";
-        }
-      ];
-    };
-  };
+  label = [
+    {
+      text = "$TIME";
+      color = "rgba(122, 162, 247, 1.0)";         # was #7AA2F7
+      font_family = "jetbrains-mono";
+      position = "0, 50";
+      font_size = "100";
+      monitor = "";
+    }
+    {
+      text = "Welcome, $USER!";
+      color = "rgba(187, 154, 247, 1.0)";         # was #BB9AF7
+      font_family = "jetbrains-mono";
+      position = "0, -50";
+      font_size = "25";
+      monitor = "";
+    }
+  ];
+};
+
+};
 }
